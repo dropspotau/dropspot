4bba330e564f693c25221350c13beb8da00fa463
alecbass <alec.bassingthwaighte@gmail.com>
2025-11-29 15:16:41 +1000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Dropspot development flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    flake-utils.url  = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config = {
+            allowUnfree = true;
+            permittedInsecurePackages = [];
+          };
+        };
+
+        rustVersion = "1.91.1";
+        rustDeps = with pkgs; [
+          rust-bin.stable.${rustVersion}.default
+        ];
+
+        editorDeps = with pkgs; [
+          neovim
+        ];
+
+        psql = (pkgs.postgresql_17.withPackages (ps: with ps; [ ]));
+        deps = with pkgs; [
+          psql
+          direnv
+        ] ++ rustDeps ++ editorDeps;
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = deps;
+
+          env = {
+            # For later when I add Postgres support
+            PGHOST = "/tmp";
+          };
+        };
+      }
+    );
+}
