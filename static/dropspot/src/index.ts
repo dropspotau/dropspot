d3d8902469c9235a801d6052ce4d155af1ee64ef
alecbass <alec.bassingthwaighte@gmail.com>
2026-02-22 18:29:03 +1000

    Calling WASM even though it fails

--- static/dropspot/src/index.ts
+++ static/dropspot/src/index.ts
@@ -5,4 +5,12 @@ import { upload_js } from "dropspot-core";
 
 console.debug("Hello", htmx);
 
-upload_js("file.txt", new Uint8Array([1, 2, 3, 4, 5]));
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", async () => {
+    console.log("Clicked");
+    const result = await upload_js("file.txt", new Uint8Array([1, 2, 3, 4, 5]));
+    console.debug(result);
+  });
+}
