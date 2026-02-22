a57e9493bb2498eda5705f44474e9ae174749baa
alecbass <alec.bassingthwaighte@gmail.com>
2026-02-22 18:11:44 +1000

    Trying to link it to Vite

--- static/dropspot/src/index.ts
+++ static/dropspot/src/index.ts
@@ -1,5 +1,8 @@
 import htmx from "htmx.org";
 import "./index.css";
 import "./my-element";
+import { upload_js } from "dropspot-core";
 
 console.debug("Hello", htmx);
+
+upload_js("file.txt", new Uint8Array([1, 2, 3, 4, 5]));
--- static/dropspot/vite.config.ts
+++ static/dropspot/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  plugins: [wasm(), topLevelAwait()],
   build: {
     lib: {
       name: "dropspot",
@@ -9,5 +12,5 @@ export default defineConfig({
       fileName: "index",
       cssFileName: "style",
     },
- },
+  },
 });
