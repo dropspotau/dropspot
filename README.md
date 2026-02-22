315017dc40ff962f9447a1c30045c843db92bb8a
alecbass <alec.bassingthwaighte@gmail.com>
2026-02-22 14:19:57 +1000

    Rebuild on change

---
+++ README.md
@@ -0,0 +1,5 @@
+### Running the local setup
+```
+bacon build-frontend
+bacon run-server
+```
--- static/dropspot/src/my-element.ts
+++ static/dropspot/src/my-element.ts
@@ -36,7 +36,7 @@ export class MyElement extends LitElement {
       <slot></slot>
       <div class="card">
         <button @click=${this._onClick} part="button">
-          count is ${this.count}
+          count is ${this.count} lmao
         </button>
       </div>
       <p class="read-the-docs">${this.docsHint}</p>
