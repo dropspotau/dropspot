a22fe027483f018aaa53e593cf29a68d106e03fb
alecbass <alec.bassingthwaighte@gmail.com>
2026-02-25 21:13:37 +1000

    Add upload circle

--- static/dropspot/src/index.ts
+++ static/dropspot/src/index.ts
@@ -1,14 +1,16 @@
 import htmx from "htmx.org";
-import "./index.css";
-import "./my-element";
 import { upload_js } from "dropspot-core";
+import "./index.css";
+import "./upload-circle.css";
+import "./utils.css";
+import "./my-element";
 
-console.debug("Hello", htmx);
+console.debug(htmx);
 
-const button = document.querySelector("button");
+const upload = document.querySelector("#upload");
 
-if (button) {
-  button.addEventListener("click", async () => {
+if (upload) {
+  upload.addEventListener("click", async () => {
     console.log("Clicked");
     const result = await upload_js("file.txt", new Uint8Array([1, 2, 3, 4, 5]));
     console.debug(result);
