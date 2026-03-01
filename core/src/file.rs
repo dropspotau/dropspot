21f39d94115239d6d4fbf0949b753d7ffc6ecc08
alecbass <alec.bassingthwaighte@gmail.com>
2026-03-01 10:37:06 +1000

    Create and parse file links with encryption

--- core/src/file.rs
+++ core/src/file.rs
@@ -1,9 +1,14 @@
+use base64::{
+    Engine,
+    alphabet::URL_SAFE,
+    engine::{GeneralPurpose, general_purpose::NO_PAD},
+};
 use serde::{Deserialize, Serialize};
+use tsify::Tsify;
 use uuid::Uuid;
 use wasm_bindgen::prelude::*;
-use tsify::Tsify;
 
-use crate::constants::ENDPOINT;
+use crate::{constants::ENDPOINT, encryption::Encryption};
 
 #[derive(Serialize, Deserialize, Tsify, Debug)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
@@ -35,4 +40,51 @@ pub async fn get_file(id: &Uuid) -> Result<File, reqwest::Error> {
         .await?;
 
     Ok(file)
+}
+
+pub fn create_link(file_id: &Uuid, encryption: &Encryption) -> String {
+    let engine = GeneralPurpose::new(&URL_SAFE, NO_PAD);
+    let key_base64 = engine.encode(&encryption.key);
+    let nonce_base64 = engine.encode(&encryption.nonce);
+
+    format!("{file_id};{key_base64};{nonce_base64}")
+}
+
+pub fn parse_link(link: &str) -> Result<(Uuid, Encryption), String> {
+    let mut parts = link.split(';');
+    let file_id = parts.next().ok_or("Missing file_id")?;
+    let key_base64 = parts.next().ok_or("Missing key")?;
+    let nonce_base64 = parts.next().ok_or("Missing nonce")?;
+
+    let engine = GeneralPurpose::new(&URL_SAFE, NO_PAD);
+    let key = engine.decode(key_base64).map_err(|e| e.to_string())?;
+    let nonce = engine.decode(nonce_base64).map_err(|e| e.to_string())?;
+
+    let file_id = Uuid::parse_str(file_id).map_err(|e| e.to_string())?;
+
+    Ok((file_id, Encryption { key, nonce }))
+}
+
+// A link to a fail. Named struct because wasm_bindgen can't return tuple structs
+#[derive(Serialize, Deserialize, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct Link {
+    file_id: String,
+    encryption: Encryption,
+}
+
+#[wasm_bindgen]
+pub fn create_link_js(file_id: String, encryption: Encryption) -> Result<String, JsError> {
+    let file_id = Uuid::parse_str(&file_id).map_err(|e| JsError::new(&format!("{e}")))?;
+    Ok(create_link(&file_id, &encryption))
+}
+
+#[wasm_bindgen]
+pub fn parse_link_js(link: &str) -> Result<Link, JsError> {
+    let (file_id, encryption) = parse_link(link).map_err(|err| JsError::new(&format!("{err}")))?;
+
+    Ok(Link {
+        file_id: file_id.to_string(),
+        encryption: encryption,
+    })
 }
--- static/dropspot/src/index.ts
+++ static/dropspot/src/index.ts
@@ -1,11 +1,15 @@
 import htmx from "htmx.org";
-import init, { upload_js, download_js } from "dropspot-core";
+import init, {
+  upload_js,
+  download_js,
+  create_link_js,
+  parse_link_js,
+} from "dropspot-core";
 
 import "./index.css";
 import "./upload-circle.css";
 import "./utils.css";
 import "./my-element";
-import { download } from "./download";
 
 console.debug(htmx);
 
@@ -37,11 +41,18 @@ if (upload instanceof HTMLElement && fileInput instanceof HTMLInputElement) {
     const fileContents = new Uint8Array(await file.arrayBuffer());
 
     const result = await upload_js(file.name, fileContents);
+
+    const link = create_link_js(result.file.id, result.encryption);
+    console.debug(link);
+    const parsed = parse_link_js(link);
+    console.debug(parsed);
+    console.debug(result);
     const buffer = (await download_js(
       result.file.id,
       result.encryption,
     )) as Uint8Array<ArrayBuffer>;
+    console.debug(buffer.length);
 
-    download(result.file.name, buffer);
+    // download(result.file.name, buffer);
   });
 }
