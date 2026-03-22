7c79f84f51db534e16bc2b6f7ff10d5d01094e34
alecbass <alec.bassingthwaighte@gmail.com>
2026-03-22 20:14:48 +1000

    Make upserting work with WASM

--- core/src/integration/gcs.rs
+++ core/src/integration/gcs.rs
@@ -1,14 +1,20 @@
+use futures_util::TryFutureExt;
 use serde::{Deserialize, Serialize};
+use tsify::Tsify;
+use wasm_bindgen::JsError;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::auth::{Authentication, get_auth_headers};
 use crate::constants::ENDPOINT;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
 pub struct UpsertGcsIntegrationPayload {
     pub bucket_name: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Tsify)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
 pub struct GcsIntegration {
     pub bucket_name: String,
 }
@@ -30,4 +36,14 @@ pub async fn upsert_gcs_integration(
         .await?;
 
     Ok(result)
+}
+
+#[wasm_bindgen]
+pub async fn upsert_gcs_integration_js(
+    payload: UpsertGcsIntegrationPayload,
+    auth: Authentication,
+) -> Result<GcsIntegration, JsError> {
+    upsert_gcs_integration(payload, auth)
+        .map_err(|e| JsError::new(&format!("{e}")))
+        .await
 }
