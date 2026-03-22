ebb5be71a848c7e69a8eaf326cef47f90a98354f
alecbass <alec.bassingthwaighte@gmail.com>
2026-03-22 16:58:01 +1000

    Move view back to being JSON

--- server/src/handlers/integration/gcs.rs
+++ server/src/handlers/integration/gcs.rs
@@ -1,10 +1,9 @@
-use axum::{Form, Json, extract::State, response::IntoResponse};
+use axum::{Json, extract::State, response::IntoResponse};
 use reqwest::StatusCode;
 use serde::Deserialize;
 
 use crate::{
     db::{User, get_organisation_for_user, upsert_gcs_integration},
-    handlers::web::{settings::UpdateSettingsTemplate, template::HtmlTemplate},
     state::AppState,
     types::ApiError,
 };
@@ -32,10 +31,9 @@ pub struct UpsertGcsIntegrationPayload {
 pub async fn handle_upsert_gcs_integration(
     State(state): State<AppState>,
     user: User,
-    Form(payload): Form<UpsertGcsIntegrationPayload>,
+    Json(payload): Json<UpsertGcsIntegrationPayload>,
 ) -> impl IntoResponse {
     let pool = state.get_pool();
-
     let organisation = get_organisation_for_user(pool, &user.id).await;
 
     if let Err(e) = organisation {
@@ -52,6 +50,5 @@ pub async fn handle_upsert_gcs_integration(
         return error.into_response();
     }
 
-    let template = UpdateSettingsTemplate { success: true };
-    HtmlTemplate(template).into_response()
+    Json(result.unwrap()).into_response()
 }
