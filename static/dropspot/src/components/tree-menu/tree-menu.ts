e2c046e73a587572d0173d4b8a12a3d31d54047d
alecbass <alec.bassingthwaighte@gmail.com>
2026-05-01 15:32:25 +1000

    Navigate between pages

--- static/dropspot/src/components/tree-menu/tree-menu.ts
+++ static/dropspot/src/components/tree-menu/tree-menu.ts
@@ -1,11 +1,15 @@
 import { html, css, LitElement } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 
 @customElement("tree-menu")
 export class TreeMenuElement extends LitElement {
   static styles = css`
     :host {
       display: contents;
+    }
+
+    slot::slotted([menu-key]:not([menu-open])) {
+      display: none !important;
     }
 
     .popover-back-button {
@@ -15,17 +19,57 @@ export class TreeMenuElement extends LitElement {
     }
   `;
 
+  @property({ attribute: "default-key" })
+  private defaultKey?: string;
+
   connectedCallback(): void {
     super.connectedCallback();
     this.addEventListener("menu-navigation", this.handleMenuChange);
+    this.addEventListener("click", this.handleClick);
+
+    if (this.defaultKey) {
+      this.showElement(this.defaultKey);
+    }
   }
 
   @state()
   private openIndex: number = 0;
 
+  private showElement = (key: string): void => {
+    const element = this.querySelector(`[menu-key="${key}"]`);
+
+    if (element instanceof HTMLElement) {
+      element.setAttribute("menu-open", "");
+    }
+
+    const otherElements = [
+      ...this.querySelectorAll(`[menu-key]:not([menu-key="${key}"])`),
+    ].filter((element) => element instanceof HTMLElement);
+
+    for (const element of otherElements) {
+      element.removeAttribute("menu-open");
+    }
+    console.debug(element);
+    console.debug(otherElements);
+  };
+
+  private handleClick = (e: MouseEvent): void => {
+    const { target } = e;
+
+    if (!(target instanceof HTMLElement)) {
+      return;
+    }
+
+    const key = target.getAttribute("menu-navigate-to");
+
+    if (key) {
+      this.showElement(key);
+    }
+  };
+
   private handleMenuChange = (e: MenuNavigationEvent): void => {
-    // @ts-ignore
-    const { name, index } = e.detail;
+    const { key } = e.detail;
+    this.showElement(key);
   };
 
   render() {
@@ -47,8 +91,7 @@ export class TreeMenuElement extends LitElement {
 }
 
 type MenuNavigationEvent = CustomEvent<{
-  name: string;
-  index: number;
+  key: string;
 }>;
 
 declare global {
