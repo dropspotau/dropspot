ef45010d78ecbe1b490464ba8e294bed99cfd047
alecbass <alec.bassingthwaighte@gmail.com>
2026-06-21 20:34:57 +1000

    Install wasm-pack too

--- scripts/deploy-build.sh
+++ scripts/deploy-build.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 export DROPSPOT_ENDPOINT="https://${DROPSPOT_ENDPOINT}"
 export DATABASE_URL="$DROPSPOT_DATABASE_URL"
 
-cargo install sqlx-cli
+cargo install sqlx-cli wasm-pack
 ./scripts/migrate-database.sh
 
 cargo build --release
