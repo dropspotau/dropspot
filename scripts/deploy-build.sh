e792efc1a3c7edd6c3f973a4f3268b8120011a44
alecbass <alec.bassingthwaighte@gmail.com>
2026-07-04 12:30:33 +1000

    Start building with optional migration

--- scripts/deploy-build.sh
+++ scripts/deploy-build.sh
@@ -2,12 +2,22 @@
 
 set -euo pipefail
 
+if [[ $# -gt 0 && $1 == "migrate" ]]; then
+    SHOULD_MIGRATE=$1
+else
+    SHOULD_MIGRATE=false
+fi
+
 # Replace the Render URL with a proper one
 export DROPSPOT_ENDPOINT="https://${DROPSPOT_ENDPOINT}"
 export DATABASE_URL="$DROPSPOT_DATABASE_URL"
 
 cargo install sqlx-cli wasm-pack
-./scripts/migrate-database.sh
+
+if [[ $SHOULD_MIGRATE == "true" ]]; then
+    echo "Migrating database"
+    ./scripts/migrate-database.sh
+fi
 
 cargo build --release
 ./scripts/build-wasm.sh
