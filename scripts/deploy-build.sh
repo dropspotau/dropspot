f2d95e64c115689bf4bffde5bd79df12c4fc45d7
alecbass <alec.bassingthwaighte@gmail.com>
2026-07-06 19:27:39 +1000

    Revert "Remove accidental Docker stuff"
    This reverts commit 543272c190757d7764885ee5296de6de2aa89e09.

--- scripts/deploy-build.sh
+++ scripts/deploy-build.sh
@@ -2,12 +2,30 @@
 
 set -euo pipefail
 
+if [[ $# -gt 0 && $1 == "migrate" ]]; then
+    if [[ $1 == "migrate" ]]; then
+        SHOULD_MIGRATE=true
+    elif [[ $1 == "no-migrate" ]]; then
+        SHOULD_MIGRATE=false
+    else 
+        echo "First argument must state \"migrate\" or \"no-migrate\""
+        exit 1
+    fi
+else
+    echo "First argument must state \"migrate\" or \"no-migrate\""
+    exit 1
+fi
+
 # Replace the Render URL with a proper one
 export DROPSPOT_ENDPOINT="https://${DROPSPOT_ENDPOINT}"
 export DATABASE_URL="$DROPSPOT_DATABASE_URL"
 
 cargo install sqlx-cli wasm-pack
-./scripts/migrate-database.sh
+
+if [[ $SHOULD_MIGRATE == "true" ]]; then
+    echo "Migrating database"
+    ./scripts/migrate-database.sh
+fi
 
 cargo build --release
 ./scripts/build-wasm.sh
