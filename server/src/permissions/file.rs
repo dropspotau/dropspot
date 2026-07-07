7ce170ff992e72ace6a2d3f991c27df467ee2193
alecbass <alec.bassingthwaighte@gmail.com>
2026-07-07 18:59:47 +1000

    Start trying to check against Organisation too

--- server/src/permissions/file.rs
+++ server/src/permissions/file.rs
@@ -1,15 +1,26 @@
-use crate::db::{File, User};
+use crate::db::{File, Organisation, User};
 
 // Whether a user can see the file
-pub fn can_see_file(file: &File, user: &User) -> bool {
-    // Admins and uploaders can view this file
+pub fn can_see_file(file: &File, organisation: &Organisation, user: Option<&User>) -> bool {
+    // Admins and anyone in the same organisation can view this file
     if file.created_by_id.is_none() {
         // Anonymous upload, anyone can view
         return true;
     }
 
+    if user.is_none() {
+        return true;
+    }
+
     // Otherwise, admins can view it
-    user.is_admin
+    let user = user.unwrap();
+    let is_same_organisation = user.organisation_id == organisation.id;
+
+    if is_same_organisation {
+        return true;
+    }
+
+    false
 }
 
 // Whether a user can edit the file
